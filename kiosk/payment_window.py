bdde4da549a16256cbc7948f7c7fd2254f754677
BSEom <ebs970725@gmail.com>
2025-03-29 23:02:51 +0900

    테스트

---
+++ kiosk/payment_window.py
@@ -0,0 +1,44 @@
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5 import uic
+
+# UI 파일 로드
+payment_form = uic.loadUiType("miniP_kiosk/ui/payment.ui")[0]
+cashPay_form = uic.loadUiType("miniP_kiosk/ui/cashPay.ui")[0]
+creditPay_form = uic.loadUiType("miniP_kiosk/ui/craditPay.ui")[0]
+
+class paymentWindow(QMainWindow, payment_form):
+    def __init__(self, menu_id, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+        self.setWindowTitle("Cafe Kiosk")
+        self.setWindowIcon(QIcon("img/coffee-cup.png"))
+
+        # 버튼 클릭 이벤트 연결
+        self.cash_btn.clicked.connect(self.openCashWindow)
+        self.credit_btn.clicked.connect(self.openCreditWindow)
+
+    def openCashWindow(self):
+        # 현금 결제 창 열기
+        self.cash_window = QMainWindow(self)
+        self.cash_ui = cashPay_form()
+        self.cash_ui.setupUi(self.cash_window)
+        self.cash_window.setWindowTitle("현금 결제")
+        self.cash_window.show()
+
+    def openCreditWindow(self):
+        # 카드 결제 창 열기
+        self.credit_window = QMainWindow(self)
+        self.credit_ui = creditPay_form()
+        self.credit_ui.setupUi(self.credit_window)
+        self.credit_window.setWindowTitle("카드 결제")
+        self.credit_window.show()
+
+        icon_path = 'miniP_kiosk/img/creditPay.png'
+        pixmap = QPixmap(icon_path)
+        if not pixmap.isNull():
+            self.credit_ui.label.setPixmap(pixmap)  # 'label'은 카드 창의 QLabel 이름
+            self.credit_ui.label.setScaledContents(True)  # 이미지 크기 조정
+
+        self.credit_window.show()
